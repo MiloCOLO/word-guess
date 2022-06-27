c1c005e1b6eaa153ed3e709d12b8ad8c8ccf2236
laurasierra17 <laura.sierra17@gmail.com>
2022-06-27 17:24:48 -0400

    timer functionality added

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 var main = document.querySelector("main");
 var countdown = 300;
 
+// Starts timer
 function startGame() {
-    
+    main.innerHTML = "";
+    var timerText = document.createElement("p");
+    main.appendChild(timerText);
+
+    var timer = setInterval(function () {
+        countdown--;
+        timerText.textContent = "Timer: " + countdown;
+    }, 1000)
 }
 
+// Pointer to the "Start Game" button
 var startBtn = document.getElementById("start-btn");
 startBtn.addEventListener("click", startGame);
