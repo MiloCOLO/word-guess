cdda0327fed226d4a3c6fc713c695cf84312ea20
laurasierra17 <laura.sierra17@gmail.com>
2022-06-27 17:33:34 -0400

    render underscores

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// variable storing word to guess
+// use the variable length to determine number of underlines
+// listen to the user's key-pressing. 
+// if their key target matches a letter, underline becomes that letter
+
+
+
+
+
+
 // Once the user clicks the "Start Game" button, the screen empties and a timer renders and starts running.
 var main = document.querySelector("main");
 var countdown = 300;
+
+var word = ['f', 'a', 'r', 'l','e','y'];
+
+
 
 // Starts timer
 function startGame() {
@@ -8,9 +22,16 @@ function startGame() {
     var timerText = document.createElement("p");
     main.appendChild(timerText);
 
+    // show underscores for every character in the word variable
+    for (var i = 0; i < word.length; i++) {
+        var underscore = document.createElement("p");
+        underscore.textContent= '_';
+        main.appendChild(underscore);
+    }
+
     var timer = setInterval(function () {
         countdown--;
-        timerText.textContent = "Timer: " + countdown;
+        timerText.textContent = "Timer: " + countdown;        
     }, 1000)
 }
 
