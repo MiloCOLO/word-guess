31e77c8d12991469ce62ea75dcf3ad211849c857
laurasierra17 <laura.sierra17@gmail.com>
2022-06-27 17:14:23 -0400

    give start button functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+// Once the user clicks the "Start Game" button, the screen empties and a timer renders and starts running.
+var main = document.querySelector("main");
+var countdown = 300;
+
+function startGame() {
+    
+}
+
+var startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", startGame);
